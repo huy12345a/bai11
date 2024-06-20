eabeea6e12bd0482e6a0b6d0f30209bf87e80783
huy12345a <Quanghuy.hmtb@gmail.com>
2024-06-20 15:54:07 +0700

    THtrienkhaiqueue

---
+++ tkqueuesudungmang/QueueClient.java
@@ -0,0 +1,20 @@
+package tkqueuesudungmang;
+
+public class QueueClient {
+    public static void main(String[] args) {
+        Myqueue queue = new Myqueue(4);
+        queue.enqueue(4);
+        queue.dequeue();
+        queue.enqueue(56);
+        queue.enqueue(2);
+        queue.enqueue(67);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(24);
+        queue.dequeue();
+        queue.enqueue(98);
+        queue.enqueue(45);
+        queue.enqueue(23);
+        queue.enqueue(435);
+    }
+}
