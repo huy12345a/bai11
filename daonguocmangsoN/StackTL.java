108626c161fcd745b4af61ff674d9a0f6d768611
huy12345a <Quanghuy.hmtb@gmail.com>
2024-06-20 11:49:54 +0700

    daonguoc

---
+++ daonguocmangsoN/StackTL.java
@@ -0,0 +1,21 @@
+package daonguocmangsoN;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class StackTL {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+     int[] array= {1,2,3,4,5};
+     int[] newArray= new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            stack.push(array[i]);
+        }
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = stack.pop();
+        }
+        System.out.println(Arrays.toString(newArray));
+
+    }
+}
